48bbe324c9cf0c48f8211aca4ea81aafb1f85c76
haru-works <harudev2525@gmail.com>
2023-02-12 21:49:20 +0900

    メモリ使用率計算の間違い修正・・・

--- vve_monitor.py
+++ vve_monitor.py
@@ -92,7 +92,7 @@ while True:
                 # プロセスあればフラグTrrue
                 process_exist_flg = True
                 # メモリ使用率計算
-                use_rate = round(int(gpu_info[3]) / int(gpu_info[2]),1) * 100
+                use_rate = round(int(gpu_info[3]) / int(gpu_info[1]),1) * 100
                 # 画面表示用
                 print(gpu_info[0] +
                       " - " + str(gpu_info[1]) + "MB" +
