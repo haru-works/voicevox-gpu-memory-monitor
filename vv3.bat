f49a75f7ab6aee681c152edd20c0bfe985dd4cf7
haru-works <harudev2525@gmail.com>
2024-02-07 19:52:15 +0900

    voicevox engine起動バッチ修正

--- vv3.bat
+++ vv3.bat
@@ -1,2 +1,2 @@
 set USERPROFILE=C:\vvappdata\vve3
-C:\voicevox_engine\0.16.0\windows-directml\run_cpu.exe --host 127.0.0.1 --port 50024
+C:\voicevox_engine\0.15.1\windows-directml\run_cpu.exe --host 127.0.0.1 --port 50024
